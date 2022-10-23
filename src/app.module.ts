582a85f28cc2cc58eddaf84c40eb37695597a895
Cesar Augusto <cesaraugustojun@gmail.com>
2022-10-23 10:08:32 -0300

    fix app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { CommentModule } from './comment/comment.module';
@@ -9,8 +11,6 @@ import { ProfileModule } from './profile/profile.module';
 import { RoleModule } from './role/role.module';
 import { RoleService } from './role/role.service';
 import { UserModule } from './user/user.module';
-import { AppService } from './app/app.service';
-import { AppController } from './app/app.controller';
 
 @Module({
   imports: [
