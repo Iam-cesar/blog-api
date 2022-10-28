7082e2b3e5ec3cc26745be48dc0fcab5a073719e
Cesar Augusto <cesaraugustojun@gmail.com>
2022-10-28 11:48:18 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*', credentials: true, allowedHeaders: '*' });
 
   const config = new DocumentBuilder()
     .setTitle('BLOG API DOC')
