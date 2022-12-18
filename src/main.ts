9c5e32c9528feaef56e7f66b4a67092f9dca063c
Cesar Augusto <cesaraugustojun@gmail.com>
2022-12-18 09:50:59 -0300

    fix: cors config

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 const PORT = process.env.PORT || 3000;
-
+const corsOptions: CorsOptions = {
+  allowedHeaders: '*',
+  maxAge: 60 * 3,
+  origin: '*',
+  preflightContinue: true,
+  methods: '*',
+  credentials: true,
+};
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({ origin: '*', credentials: true, allowedHeaders: '*' });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptions);
 
   const config = new DocumentBuilder()
     .setTitle('BLOG API DOC')
